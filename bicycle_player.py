ab6c97b304253554d2e71ea20fc9b2f07a32417a
netanelf <netanelf@gmail.com>
2017-03-16 22:22:55 +0200

    BicyclePlayer can be initiated with 'overlay_debug=True' and log debug messages to overlay  image and text overlays are saved in dictionaries to support few overlays at a time

--- bicycle_player.py
+++ bicycle_player.py
@@ -11,10 +11,11 @@ __author__ = 'netanel'
 
 
 class BicyclePlayer(threading.Thread):
-    def __init__(self, input_file):
+    def __init__(self, input_file, overlay_debug=False):
         self.logger = logging.getLogger(self.__class__.__name__)
         self.logger.info('initializing {}, input file: {}'.format(self.__class__.__name__, input_file))
         super(BicyclePlayer, self).__init__()
+        self.overlay_debug = overlay_debug
         self.input_file = input_file
         self.video_capturer = None
         self.frame_rate = None
@@ -23,9 +24,16 @@ class BicyclePlayer(threading.Thread):
         self.video_resolution = None
         self.should_run = False
         self.paused = False
-        self.overlaid_image = None
-        self.overlay_coordinates = None
-        self.overlay_transparency = None
+
+        # image overlay placeholders
+        self.image_overlays = {}
+
+        # text overlay placeholders
+        self.text_overlays = {}
+        # self.overlaid_text = None
+        # self.text_overlay_coordinates = None
+        self.text_font = cv2.FONT_HERSHEY_SIMPLEX
+
         self.frame_counter = 0
         self._read_file()
 
@@ -72,13 +80,20 @@ class BicyclePlayer(threading.Thread):
                 self.video_capturer.set(cv2.cv.CV_CAP_PROP_POS_AVI_RATIO, 0)
                 continue
 
-            if self.overlaid_image is not None and self.overlay_transparency is not None and self.overlay_coordinates is not None:
-                x_start, y_start = self.overlay_coordinates
-                (x_dim, y_dim, z_dim) = self.overlaid_image.shape
-                base_partial = frame.copy()[x_start: x_start + x_dim, y_start: y_start + y_dim, :]
-                overlaid_output = self.overlaid_image.copy()
-                cv2.addWeighted(self.overlaid_image, self.overlay_transparency, base_partial, 1 - self.overlay_transparency, 0, overlaid_output)
-                frame[x_start: x_start + x_dim, y_start: y_start + y_dim, :] = overlaid_output
+            if len(self.image_overlays) > 0:
+                for overlay_name, overlay_data in self.image_overlays.items():
+                    image = overlay_data['image']
+                    transparency = overlay_data['transparency']
+                    x_start, y_start = overlay_data['location']
+                    (x_dim, y_dim, z_dim) = image.shape
+                    base_partial = frame.copy()[x_start: x_start + x_dim, y_start: y_start + y_dim, :]
+                    overlaid_output = image.copy()
+                    cv2.addWeighted(image, transparency, base_partial, 1 - transparency, 0, overlaid_output)
+                    frame[x_start: x_start + x_dim, y_start: y_start + y_dim, :] = overlaid_output
+
+            if len(self.text_overlays) > 0:
+                for overlay_name, overlay_data in self.text_overlays.items():
+                    cv2.putText(frame, overlay_data['text'], overlay_data['location'], self.text_font, 1, (255, 255, 255), 2)
 
             cv2.imshow('BicycleVideoWindow', frame)
             self.frame_counter += 1
@@ -114,6 +129,10 @@ class BicyclePlayer(threading.Thread):
         if delay == 0:
             self.logger.error('speed to high')
             return
+
+        if self.overlay_debug:
+            self.text_overlays['debug_message'] = {'text': 'speed: {}'.format(speed), 'location': (50, 500)}
+
         self.delay = delay
 
     def get_speed(self):
@@ -140,7 +159,8 @@ class BicyclePlayer(threading.Thread):
         self.logger.debug('ramp_steps: {}, ramp_time_step: {}, ramp_speed_step: {}'
                           .format(ramp_steps, ramp_time_step, ramp_speed_step))
         for step in range(1, ramp_steps + 1):
-            self.set_speed(speed=start_speed + step * ramp_speed_step)
+            s = start_speed + step * ramp_speed_step
+            self.set_speed(speed=s)
             time.sleep(ramp_time_step)
 
     def stop_playing(self):
@@ -161,11 +181,12 @@ class BicyclePlayer(threading.Thread):
         self.paused = not self.paused
         self.logger.debug('pause: {}'.format(self.paused))
 
-    def overlay_image(self, image, location, weight_transparency=1):
+    def overlay_image(self, image, location, name, weight_transparency=1):
         """
         overlay the given image in some location from now until a newer image is given or disable_overlay is called.
         :param image: n*m*3 image rgb matrix (numpy.ndarray)
         :param location: [x, y]
+        :param name: name for this overlay - so we can remove this overlay when we want
         :param weight_transparency: 0 - overlaid image is transparent, 1: opaque
         :return:
         """
@@ -178,17 +199,25 @@ class BicyclePlayer(threading.Thread):
             self.logger.error('overlaid image to big: {}, (video size: {})'.format(image.shape, self.video_resolution))
             return
 
-        self.overlaid_image = image
-        self.overlay_coordinates = location
-        self.overlay_transparency = weight_transparency
-        self.logger.debug('created overlay image with dimensions: {}'.format(self.overlaid_image.shape))
+        self.image_overlays[name] = {'image': image, 'location': location, 'transparency': weight_transparency}
+        self.logger.debug('created overlay image with dimensions: {}'.format(self.image_overlays[name]['image'].shape))
 
-    def disable_overlay(self):
-        self.logger.info('disable_overlay was called')
-        self.overlaid_image = None
-        self.overlay_transparency = None
-        self.overlay_coordinates = None
+    def disable_image_overlay(self, name):
+        self.logger.info('disable_overlay was called, name: {}'.format(name))
+        self.image_overlays.pop(name)
 
+    def overlay_text(self, text, location, name):
+        """
+        overlay text
+        :param text: the wanted text
+        :param location: (x, y) ccordinates
+        :param name: name of this overlay so we can remove it later
+        :return:
+        """
+        self.text_overlays[name] = {'text': text, 'location': tuple(location)}
+
+    def disable_text_overlay(self, name):
+        self.text_overlays.pop(name)
 
 def init_logging(logger_name, logger_level):
     logger = logging.getLogger()
@@ -211,21 +240,22 @@ if __name__ == '__main__':
     init_logging(logger_name='bicyclePlayer', logger_level=logging.DEBUG)
     f = 'movie.mp4'
     image = cv2.imread("icon.png")
-    #image = cv2.imread("1.jpeg")
-    #f = 'V70309-235541.mp4'
-    player = BicyclePlayer(input_file=f)
+    player = BicyclePlayer(input_file=f, overlay_debug=True)
     player.setDaemon(True)
     player.start()
     time.sleep(10)
-    player.overlay_image(image=image, location=[50, 50], weight_transparency=0.5)
+    player.overlay_image(image=image, location=[50, 50], name='icon',weight_transparency=0.5)
 
-    player.ramp_speed(start_speed=3, stop_speed=0.2, ramp_time=timedelta(seconds=30))
-    player.disable_overlay()
+    player.ramp_speed(start_speed=3, stop_speed=0.2, ramp_time=timedelta(seconds=15))
+    player.overlay_image(image=image, location=[200, 200], name='icon2', weight_transparency=0.75)
+
     player.set_speed(speed=2)
     time.sleep(10)
+    player.disable_image_overlay(name='icon')
     player.set_speed(speed=0.5)
     time.sleep(10)
     player.set_speed(speed=1)
+    player.disable_image_overlay(name='icon2')
     time.sleep(10)
     player.stop_playing()
     time.sleep(5)
