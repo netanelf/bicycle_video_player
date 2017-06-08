2954fbad25b601d1c7d9aeba31cb4c071d4db59c
Omer Kapach <omerkap@gmail.com>
2017-06-09 00:09:13 +0300

    update history_controller bug fixes

--- history_exhibit/history_controller.py
+++ history_exhibit/history_controller.py
@@ -1,32 +1,38 @@
 import time
 
+import logging
+
 from main_controller import VlcPlayer
 import config
 
 
 class HistoryController(VlcPlayer):
+
     MOVIE = "movie.mp4"
 
     def __init__(self):
         super(HistoryController, self).__init__()
         self.thresh = config.THRESHOLD
         self.load_movie(self.MOVIE)
-        self.d={}
+        self.speeds={}
+        self._logger = logging.getLogger(self.__class__.__name__)
+
 
     def run(self):
         while True:
             time.sleep(0.1)
-            toggle = 0
-            for key, value in self.d.iteritems():
+            toggle = False
+            for key, value in self.speeds.iteritems():
                 if value > self.thresh:
-                    toggle = 1
+                    toggle = True
                     break
-            if toggle == 1 :
+            if toggle and not self._is_playing:
                 self.play()
-            else:
+                self._logger.debug('im toggling on!')
+            elif not toggle and self._is_playing:
+                self._logger.debug('im toggling off!')
                 self.pause()
 
-
-
     def update_encoder(self, player_id, encoder_data):
-        self.d[player_id] = encoder_data*config.CONVERSION_FACTOR
+        self._logger.debug('player id {}'.format(player_id))
+        self.speeds[player_id] = encoder_data * config.CONVERSION_FACTOR[player_id]
