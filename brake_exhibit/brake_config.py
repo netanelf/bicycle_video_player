54a1769af4af0c6663b7d2d63dee93dda4dd4a20
Or Levi <orlevi1234@gmail.com>
2017-06-09 00:11:25 +0300

    basic functionality working

---
+++ brake_exhibit/brake_config.py
@@ -0,0 +1,11 @@
+THRESHOLD = {"SPEED_TH_1": 50,
+             "SPEED_TH_2": 100,
+             "SPEED_TH_3": 200,
+             "SPEED_TH_4": 300}
+
+VID_SPEED = {"SPEED_TH_1": 0.3,
+             "SPEED_TH_2": 0.5,
+             "SPEED_TH_3": 0.8,
+             "SPEED_TH_4": 1.0}
+
+MOVIE = r"brake_exhibit/MTB Haruvit 08 04 2016.avi"
--- brake_exhibit/brake_controller.py
+++ brake_exhibit/brake_controller.py
@@ -1,37 +1,39 @@
+from json import encoder
+
 from main_controller import VlcPlayer
-import config as cfg
+import time
+import brake_config as cfg
 
 class BrakeController(VlcPlayer):
-    CFG = 0 #todo
-    MOVIE = "" #todo
 
-    def __init__(self,serial):
-        super(self, self.init())
-        self.thresh = self.CFG
-        self.load_movie(self.MOVIE)
-
-    def do_kaftor(kaftor_number):
-        pass
+    def __init__(self):
+        super(BrakeController, self).__init__()
+        self._encoder_data = 0
+        self._prev_video_speed = 0
+        self.load_movie(cfg.MOVIE)
 
     def run(self):
-        while True:
-            pass
-            # use self.speed
-
-    def has_send(self):
-        pass
-        # read time/ frame
-
-    def do_kaftor(self,kaftor_number):
-        #TODO
-        pass
+        threshold_passed = False
+        while self._alive:
+            for key in sorted(cfg.THRESHOLD.keys(), reverse=True):
+                if self._encoder_data >= cfg.THRESHOLD[key]:
+                    if self._prev_video_speed != cfg.VID_SPEED[key]:
+                        self.update_speed(cfg.VID_SPEED[key])
+                        self._prev_video_speed = cfg.VID_SPEED[key]
+                    if not self._is_playing:
+                        self.play()
+                    threshold_passed = True
+                    break
+            if not threshold_passed:
+                if self._is_playing:
+                    self.pause()
+            threshold_passed = False
+            time.sleep(0.05)
 
     def update_encoder(self, player_id, encoder_data):
-        #TODO
-        pass
+        self._encoder_data = encoder_data
 
 if __name__ =='__main__':
-    init_logging(log_name='main_controller', logger_level=logging.INFO)
     b = BrakeController()
     while b.player.is_alive():
         time.sleep(1)
