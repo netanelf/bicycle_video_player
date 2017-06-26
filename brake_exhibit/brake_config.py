bdce9f9aaf4f9be9ef52a4e5d0a993a00dee7c9d
Or Levi <orlevi1234@gmail.com>
2017-06-26 18:42:19 +0300

    minor updates + switch movie function

--- brake_exhibit/brake_config.py
+++ brake_exhibit/brake_config.py
@@ -8,4 +8,4 @@ VID_SPEED = {"SPEED_TH_1": 0.3,
              "SPEED_TH_3": 0.8,
              "SPEED_TH_4": 1.0}
 
-MOVIE = r"brake_exhibit/MTB Haruvit 08 04 2016.avi"
+MOVIE1 = r"brake_exhibit/MTB Haruvit 08 04 2016.avi"
--- brake_exhibit/brake_controller.py
+++ brake_exhibit/brake_controller.py
@@ -7,10 +7,12 @@ import brake_config as cfg
 class BrakeController(VlcPlayer):
 
     def __init__(self):
-        super(BrakeController, self).__init__()
+        super(BrakeController, self).__init__(num_of_mps=1)
         self._encoder_data = 0
         self._prev_video_speed = 0
-        self.load_movie(cfg.MOVIE)
+        self._cur_movie = 0
+        self.load_movie(cfg.MOVIE1,0)
+        self.set_fullscreen(0)
 
     def run(self):
         threshold_passed = False
@@ -20,20 +22,51 @@ class BrakeController(VlcPlayer):
                     if self._prev_video_speed != cfg.VID_SPEED[key]:
                         self.update_speed(cfg.VID_SPEED[key])
                         self._prev_video_speed = cfg.VID_SPEED[key]
-                    if not self._is_playing:
-                        self.play()
+                    if not self.is_playing(self._cur_movie):
+                        self.play(self._cur_movie)
                     threshold_passed = True
                     break
             if not threshold_passed:
-                if self._is_playing:
-                    self.pause()
+                if self.is_playing(self._cur_movie):
+                    self.pause(self._cur_movie)
             threshold_passed = False
             time.sleep(0.05)
 
     def update_encoder(self, player_id, encoder_data):
         self._encoder_data = encoder_data
 
+    def switch_movie(self, old_movie, new_movie):
+        self.pause(old_movie)
+        self.play(new_movie)
+        self._cur_movie = new_movie
+
+"""
+class Try(VlcPlayer):
+
+    def __init__(self):
+        super(Try, self).__init__(num_of_mps=2)
+        self._cur_movie = 0
+        self.load_movie(cfg.MOVIE4,0)
+        self.load_movie(cfg.MOVIE5,1)
+
+    def switch_movie(self, old_movie, new_movie):
+        self.pause(old_movie)
+        self.play(new_movie)
+        self._cur_movie = new_movie
+
 if __name__ =='__main__':
-    b = BrakeController()
-    while b.player.is_alive():
-        time.sleep(1)
+    b = Try()
+    b.set_fullscreen(media_sel=0)
+    b.set_fullscreen(media_sel=1)
+    not_switched = True
+    b.play(b._cur_movie)
+    while (not_switched):
+        time.sleep(0.05)
+        if b.get_time(b._cur_movie) > 18000:
+            print "switching movie"
+            b.switch_movie(0,1)
+            not_switched = False
+    time.sleep(20)
+
+    """
+
