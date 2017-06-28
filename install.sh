d941795f605db7dff12d02f9deaeddfbba069c36
KeepKipod <keepkipod@gmail.com>
2017-06-28 21:39:34 +0300

    First time install bash added

---
+++ install.sh
@@ -0,0 +1,14 @@
+sudo apt-get update -y
+sudo apt-get install git -y
+sudo apt-get install python-pip -y
+sudo apt-get install python-numpy -y
+sudo apt-get install python-serial -y
+sudo apt-get install python-opencv -y
+sudo usermod -a -G dialout $USER
+cd ~/
+git clone https://github.com/netanelf/bicycle_video_player
+cd ~/bicycle_video_player
+chmod +x achbar.sh
+cp run_race_player.desktop ~/.config/autostart
+echo "Installation completed. Restarting in 5 seconds"
+shutdown -r -t 5
