611c6005c6309fd579c337f497d316709d48f92e
KeepKipod <keepkipod@gmail.com>
2017-06-28 22:19:35 +0300

    Fixed install.sh file

--- install.sh
+++ install.sh
@@ -1,14 +1,19 @@
 sudo apt-get update -y
+sudo apt-get install python-serial -y
+sudo apt-get install vlc -y
 sudo apt-get install git -y
 sudo apt-get install python-pip -y
 sudo apt-get install python-numpy -y
-sudo apt-get install python-serial -y
 sudo apt-get install python-opencv -y
+sudo apt-get install python-serial -y
+sudo apt-get install vlc -y
 sudo usermod -a -G dialout $USER
 cd ~/
 git clone https://github.com/netanelf/bicycle_video_player
 cd ~/bicycle_video_player
 chmod +x achbar.sh
+mkdir ~/.config
+mkdir ~/.config/autostart
 cp run_race_player.desktop ~/.config/autostart
-echo "Installation completed. Restarting in 5 seconds"
-shutdown -r -t 5
+echo "Installation completed. Restarting in 1 minute"
+shutdown -r -t 1
