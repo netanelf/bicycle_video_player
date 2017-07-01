d0753edaa3119000d90217e4e3e6576ed3b56e8c
netanel <netanelf@gmail.com>
2017-07-01 12:42:41 +0300

    [Omer] added default time to restart the movie, loop when reaching the end, a default conversion factor for all and support to unlimited number of connected devices

---
+++ history_exhibit/history_config.py
@@ -0,0 +1,4 @@
+MOVIE_FILENAME = "history.mp4"
+THRESHOLD = 10
+CONVERSION_FACTOR = 1  # TODO
+HOLD_BEFORE_RESTART = 15  # [S] time to wait before resetting the scene. 0 for cancelling the option.
--- history_exhibit/history_controller.py
+++ history_exhibit/history_controller.py
@@ -1,19 +1,21 @@
 from main_controller import VlcPlayer
 import logging
-import config as cfg
+import history_config as cfg
 import time
 import os
 
 
 class HistoryController(VlcPlayer):
+
     def __init__(self):
         super(HistoryController, self).__init__()
         self.thresh = cfg.THRESHOLD
-        movie_path = os.path.join(os.path.basename(os.path.dirname(os.path.realpath(__file__))), cfg.MOVIE_FILENAME)
-        self.load_movie(movie_path)
+        self.movie_path = os.path.join(os.path.basename(os.path.dirname(os.path.realpath(__file__))), cfg.MOVIE_FILENAME)
+        self.load_movie(self.movie_path)
         self.speeds = {}
         self._logger = logging.getLogger(self.__class__.__name__)
         self.set_fullscreen()
+        self._movie_stopped_time = time.time()
 
     def run(self):
         self.play()
@@ -32,7 +34,45 @@ class HistoryController(VlcPlayer):
             elif not toggle and self.is_playing():
                 self._logger.debug('im toggling off!')
                 self.pause()
+                self._movie_stopped_time = time.time()
+            if self.is_playing() is False \
+                    and (time.time() - self._movie_stopped_time > cfg.HOLD_BEFORE_RESTART):
+                self._logger.debug('bicycle idle for {} time, returning to default scene'.format(
+                    time.time() - self._movie_stopped_time))
+                self.set_position(0)
+            self.restart_ended_video(self.movie_path, 0)
 
     def update_encoder(self, player_id, encoder_data):
         self._logger.debug('player id: {} data: {}'.format(player_id, encoder_data))
-        self.speeds[player_id] = encoder_data * cfg.CONVERSION_FACTOR[player_id]
+        self.speeds['{}'.format(player_id)] = encoder_data * cfg.CONVERSION_FACTOR
+
+    def do_kaftor(self, kaftor_number):
+        pass
+
+if __name__ == '__main__':
+
+    p = HistoryController()
+    p.setDaemon(True)
+    p.start()
+    time.sleep(2)
+    print("a")
+    p.update_encoder(player_id=2, encoder_data=3000)
+    time.sleep(2)
+    print("a")
+    p.update_encoder(player_id=1, encoder_data=1001)
+    time.sleep(2)
+    print("a")
+    p.update_encoder(player_id=2, encoder_data=0)
+    time.sleep(2)
+    print("a")
+    p.update_encoder(player_id=0, encoder_data=100)
+    time.sleep(2)
+    print("a")
+    p.update_encoder(player_id=1, encoder_data=0)
+    time.sleep(2)
+    print("a")
+    p.update_encoder(player_id=0, encoder_data=0)
+    time.sleep(4)
+    print("a")
+    p.update_encoder(player_id=0, encoder_data=11)
+    time.sleep(300)
