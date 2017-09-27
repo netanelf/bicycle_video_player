07b4d020265d91394f617ef59dd3c50093bb5df3
netanelf <netanelf@gmail.com>
2017-09-27 20:50:07 +0300

    ensure installer script checks out a constant release version, when we want to release a new version, update script

--- install.sh
+++ install.sh
@@ -9,7 +9,7 @@ sudo apt-get install python-serial -y
 sudo apt-get install vlc -y
 sudo usermod -a -G dialout $USER
 cd ~/
-git clone https://github.com/netanelf/bicycle_video_player
+git clone https://github.com/netanelf/bicycle_video_player -b v1.2
 cd ~/bicycle_video_player
 chmod +x achbar.sh
 mkdir ~/.config
