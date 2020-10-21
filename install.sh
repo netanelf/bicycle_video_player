90c10e69ec24ccf159168a6a5d0ae2e8c67e2352
netanelf <netanelf@gmail.com>
2020-10-21 21:25:36 +0300

    Updated install.sh as per request. was not tested by me

--- install.sh
+++ install.sh
@@ -1,19 +1,27 @@
-sudo apt-get update -y
-sudo apt-get install python-serial -y
-sudo apt-get install vlc -y
-sudo apt-get install git -y
-sudo apt-get install python-pip -y
-sudo apt-get install python-numpy -y
-sudo apt-get install python-opencv -y
-sudo apt-get install python-serial -y
-sudo apt-get install vlc -y
-sudo usermod -a -G dialout $USER
-cd ~/
-git clone https://github.com/netanelf/bicycle_video_player -b v1.4
+#!/bin/bash
+
+if (( EUID == 0 )); then
+	echo 'Please run without sudo!' 1>&2
+	exit 1
+fi
+
+cd "$(dirname "${BASH_SOURCE[0]}")"
+
+sudo apt update
+sudo apt install -y git python2 vlc htop
+
+wget -qO- https://bootstrap.pypa.io/get-pip.py | sudo python2
+
+pip2 install numpy pyserial opencv-python==3.4.8.29 # ==3.4.0.14
+
+cd ~
+git clone https://github.com/netanelf/bicycle_video_player.git
 cd ~/bicycle_video_player
 chmod +x achbar.sh
-mkdir ~/.config
-mkdir ~/.config/autostart
-cp run_race_player.desktop ~/.config/autostart
+mkdir -p ~/.config/autostart/
+cp run_race_player.desktop ~/.config/autostart/
+
+sudo usermod -a -G dialout $USER
+
 echo "Installation completed. Restarting in 1 minute"
 shutdown -r -t 1
